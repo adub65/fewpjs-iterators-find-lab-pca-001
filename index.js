8c33b4a7c79706bd06f82b8efc68a9551b5e58c9
Aaron Wilson <adub65@gmail.com>
2020-07-18 10:20:12 -0400

    find basics

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(teamArray) {
+  let winner = (teamArray.find(team => team.result === "W"))
+  if (winner) {
+    return winner.year
+  } else {
+    return undefined
+  }
+}
